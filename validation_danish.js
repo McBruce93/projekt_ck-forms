59d401735a792620d4f9db408ec269196d27831e
qiTsuk <kasper.andreasen@gmail.com>
2018-10-04 17:29:43 +0200

    Getting there!!

---
+++ validation_danish.js
@@ -0,0 +1,67 @@
+$(function () {
+    var requiredCheckboxes = $('.checkbox-group1 :checkbox[required]');
+    requiredCheckboxes.change(function () {
+        if (requiredCheckboxes.is(':checked')) {
+            requiredCheckboxes.removeAttr('required');
+        } else {
+            requiredCheckboxes.attr('required', 'required');
+        }
+    });
+});
+$(function () {
+    var requiredCheckboxes = $('.checkbox-group2 :checkbox[required]');
+    requiredCheckboxes.change(function () {
+        if (requiredCheckboxes.is(':checked')) {
+            requiredCheckboxes.removeAttr('required');
+        } else {
+            requiredCheckboxes.attr('required', 'required');
+        }
+    });
+});
+
+function firstNameCheck() {
+    var firstName = document.querySelector("#first_name");
+    if (firstName.value.length < 1) {
+        firstName.setCustomValidity("Du skal indtaste dit fornavn.");
+    } else {
+        firstName.setCustomValidity("");
+    }
+}
+
+function lastNameCheck() {
+    var lastName = document.querySelector("#last_name");
+    if (lastName.value.length < 1) {
+        lastName.setCustomValidity("Du skal indtaste dit efternavn."); 
+    } else {
+        lastName.setCustomValidity("");
+    }
+}
+
+function emailCheck() {
+    var email = document.querySelector("#email");
+    if (email.value.length < 1) {
+        email.setCustomValidity("Du skal indtaste din email.");
+    } else {
+        email.setCustomValidity("");
+    }
+}
+
+function phoneNumberCheck() {
+    var phoneNumber = document.querySelector("#phone_number");
+    if (phoneNumber.value.length < 1) {
+        phoneNumber.setCustomValidity("Du skal indtaste dit telefon nummer.");
+    } else {
+        phoneNumber.setCustomValidity("");
+    }
+}
+
+
+function aboutCheck() {
+    console.log("Halloo??");
+    var about = document.querySelector("#about_you");
+    if (about.value.length < 1) {
+        about.setCustomValidity("Du skal skrive lidt om dig selv.");
+    } else {
+        about.setCustomValidity("");
+    }
+}
--- volunteer.php
+++ volunteer.php
@@ -1,19 +1,11 @@
 <?php
 include_once 'dbhandler.php';
-if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email'])
-    && isset($_POST['phone']) && isset($_POST['day']) && isset($_POST['job'])) {
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $days[] = $_POST['day[]'];
-    $job[] = $_POST['job[]'];
-} else {
-    echo "<script>
-            alert('Du skal udfylde alle felterne');
-            window.location.href='../volunteer_form_danish.html';
-        </script>";
-}
+    $phone = $_POST['phone_number'];
+    $days[] = $_POST['day'];
+    $job[] = $_POST['job'];
 
 function testEmail($email)
 {
