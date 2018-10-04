a067f37ea5cf772c6d942c6cad89e24fd0e0285a
qiTsuk <kasper.andreasen@gmail.com>
2018-10-04 15:01:59 +0200

    PHP ændringer!

--- volunteer.php
+++ volunteer.php
@@ -9,8 +9,20 @@ if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['e
     $days[] = $_POST['day'];
     $job[] = $_POST['job'];
 } else {
-    echo"<script>
+    echo "<script>
             alert('Du skal udfylde alle felterne');
             window.location.href='../volunteer_form_danish.html';
         </script>";
+}
+
+function testEmail($email)
+{
+    $testEmailQuery = "SELECT * FROM Volunteers WHERE Email='$email';";
+    $result = mysqli_query($dbc, $testEmailQuery);
+    if (!$row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        echo "<script>
+            alert('Der er allerede oprettet en frivillig, med denne email. Såfremt du vil ændre noget, skal du kontakte system admin (indsæt rigtig email her)');
+            window.location.href='../volunteer_form_danish.html';
+        </script>";
+    }
 }
