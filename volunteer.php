55685e8dd2040bd8a89e45b90a4cfa34d06ebd61
qiTsuk <kasper.andreasen@gmail.com>
2018-10-05 16:35:50 +0200

    Works now!

--- volunteer.php
+++ volunteer.php
@@ -1,20 +1,20 @@
 <?php
-include_once 'dbhandler.php';
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone_number'];
-    $days[] = $_POST['day'];
-    $job[] = $_POST['job'];
+include_once 'dbconn.php';
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$phone = $_POST['phone_number'];
+$days[] = $_POST['day'];
+$job[] = $_POST['job'];
 
-function testEmail($email)
-{
-    $testEmailQuery = "SELECT * FROM Volunteers WHERE Email='$email';";
-    $result = mysqli_query($dbc, $testEmailQuery);
-    if (!$row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-        echo "<script>
-            alert('Der er allerede oprettet en frivillig, med denne email. Såfremt du vil ændre noget, skal du kontakte system admin (indsæt rigtig email her)');
-            window.location.href='../volunteer_form_danish.html';
-        </script>";
-    }
+
+$testEmailQuery = "SELECT * FROM Volunteers WHERE Email='$email';";
+$result = mysqli_query($nutzyjco_33ef8329, $testEmailQuery);
+if ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    echo "<script>
+        alert('Der er allerede oprettet en frivillig, med denne email. Såfremt du vil ændre noget, skal du kontakte system admin (indsæt rigtig email her)');
+        window.location.href='../volunteer_form_danish.html';
+    </script>";
+} else {
+    echo "Something!!";
 }
