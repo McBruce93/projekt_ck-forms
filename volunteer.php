d991a543cb15dac688eff38672d1f65970b5bf63
qiTsuk <kasper.andreasen@gmail.com>
2018-10-05 16:53:34 +0200

    Har lavet lidt om på strukturen. Nu bruger jeg prepared statements hvilket jeg gør fremover, så vi er sikrede mod SQL Injection.

--- volunteer.php
+++ volunteer.php
@@ -7,14 +7,19 @@ $phone = $_POST['phone_number'];
 $days[] = $_POST['day'];
 $job[] = $_POST['job'];
 
+$stmt = $nutzyjco_33ef8329->prepare("SELECT * FROM Volunteers WHERE Email=?");
+$stmt->bind_param("s", $email);
 
-$testEmailQuery = "SELECT * FROM Volunteers WHERE Email='$email';";
-$result = mysqli_query($nutzyjco_33ef8329, $testEmailQuery);
-if ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+$stmt->execute();
+$result = $stmt->get_result();
+if ($result->num_rows !== 0) {
     echo "<script>
-        alert('Der er allerede oprettet en frivillig, med denne email. Såfremt du vil ændre noget, skal du kontakte system admin (indsæt rigtig email her)');
-        window.location.href='../volunteer_form_danish.html';
-    </script>";
+            alert('Der er allerede oprettet en frivillig, med denne email. Såfremt du vil ændre noget, skal du kontakte system admin (indsæt rigtig email her)');
+            window.location.href='../volunteer_form_danish.html';
+        </script>";
 } else {
-    echo "Something!!";
+    
+    echo "It works!";
 }
+$stmt->close();
+
