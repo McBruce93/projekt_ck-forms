a12def565a1f3c6585a33c790f94f7bf52f563dd
qiTsuk <kasper.andreasen@gmail.com>
2018-10-10 13:00:42 +0200

    Changes! Plis work!

--- sponsor_forms/sponsor.php
+++ sponsor_forms/sponsor.php
@@ -14,7 +14,6 @@ $target_dir = "wp-content/uploads/";
 $target_file = $target_dir . basename($_FILES['image_upload']["name"]);
 $uploadOK = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-$upload_num = 1;
 
 if (isset($_POST["send"])) {
     $check = getimagesize($_FILES["image_upload"]["tmp_name"]);
@@ -34,9 +33,8 @@ if ($uploadOK == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["image_upload"]["tmp_name"], SITE_ROOT . '\uploads\\' . basename($_FILES['image_upload']['name']))) {
+    if (move_uploaded_file($_FILES["image_upload"]["tmp_name"], SITE_ROOT . '/uploads/' . basename($_FILES['image_upload']['name']))) {
         echo "The file ". basename( $_FILES["image_upload"]["name"]). " has been uploaded.";
-        $upload_num++;
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
