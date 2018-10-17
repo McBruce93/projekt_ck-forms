b5a5a60de3ebb6eacc43030d93d9444ed8de6ede
qiTsuk <kasper.andreasen@gmail.com>
2018-10-17 16:30:09 +0200

    Fixed the DB link!

--- volunteer_forms/volunteer.php
+++ volunteer_forms/volunteer.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'wp-content/plugins/forms_plugin/dbconn.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/projekt_ck/wp-content/plugins/forms_plugin/dbconn.php';
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $email = $_POST['email'];
